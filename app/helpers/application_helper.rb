bbbf6dc5b19d83eb8185154f28efa27d2fdd3659
mychi15 <mcyeekyun@gmail.com>
2020-02-07 03:57:53 +0300

    fix copy button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def domain
-    Rails.env.development? ? 'localhost:3000' : 'shorttyurl.herokuapp.com/'
+    Rails.env.development? ? 'localhost:3000' : 'shorttyurl.herokuapp.com'
   end
 end
--- app/javascript/copy.js
+++
@@ -1,6 +0,0 @@
-function copy() {
-  var copyText = document.getElementById("copy_url");
-  copyText.select();
-  document.execCommand("copy");
-  alert("Copied the text: " + copyText.value);
-}
